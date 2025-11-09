740b4d78e1624df5953c752bd102f3b297aa369a
Bayron <yentvillega@gmail.com>
2025-11-09 01:20:01 -0500

    fix baseUrl

--- src/settings/apiConection.js
+++ src/settings/apiConection.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiCS = axios.create({
-    baseURL:"http://localhost:3000/api",
+    baseURL:"https://citysafex.onrender.com//api",
     headers: {
         "Content-Type": "application/json",
     },
